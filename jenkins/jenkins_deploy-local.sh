eea20116d86a3175e4215a3fc5827c754f324e01
Robert Kent <git@rooted.ltd.uk>
2018-01-23 16:59:45 +0000

    Start adding slave functionality

--- jenkins/jenkins_deploy-local.sh
+++ jenkins/jenkins_deploy-local.sh
@@ -17,7 +17,7 @@ ROOT_GROUP="${ROOT_USER:-wheel}"
 JENKINS_USER="${JENKINS_USER:-jenkins}"
 JENKINS_GROUP="${JENKINS_GROUP:-jenkins}"
 
-whoami | grep -Eq "^$ROOT_USER$" || FATAL This script MUST be run as $ROOT_USER
+whoami | grep -Eq "^$ROOT_USER$" || FATAL "This script MUST be run as $ROOT_USER"
 
 LOG_ROTATE_COUNT='10'
 LOG_ROTATE_SIZE='10m'
@@ -36,6 +36,14 @@ for i in `seq 1 $#`; do
 	case "$1" in
 		-n|--name)
 			JENKINS_APPNAME="$2"
+			shift 2
+			;;
+		--master_url)
+			JENKINS_MASTER_URL="$2"
+			shift 2
+			;;
+		--master-jnlp-port)
+			JENKINS_JNLP_PORT="$2"
 			shift 2
 			;;
 		-r|--release-type)
@@ -101,75 +109,100 @@ INSTALL_BASE_DIR="${INSTALL_BASE_DIR:-/opt}"
 DEPLOYMENT_DIR="$INSTALL_BASE_DIR/$JENKINS_APPNAME"
 
 if [ -z "$JENKINS_CONFIG_SEED_REPO" ]; then
-	FATAL No JENKINS_CONFIG_SEED_REPO provided
+	FATAL 'No JENKINS_CONFIG_SEED_REPO provided'
 fi
 
 if [ -z "$JENKINS_SCRIPTS_REPO" ]; then
-	FATAL No JENKINS_SCRIPTS_REPO provided
+	FATAL 'No JENKINS_SCRIPTS_REPO provided'
 fi
 
-[ -d "$DEPLOYMENT_DIR" ] && FATAL Deployment "$DEPLOYMENT_DIR" already exists, please remove
+[ -d "$DEPLOYMENT_DIR" ] && FATAL "Deployment '$DEPLOYMENT_DIR' already exists, please remove"
+
 
 INFO "Creating $DEPLOYMENT_DIR layout"
-mkdir -p "$DEPLOYMENT_DIR"/{bin,config,.ssh,.git} "/var/log/$JENKINS_APPNAME"
+mkdir -p "$DEPLOYMENT_DIR"/{bin,config} "/var/log/$JENKINS_APPNAME"
 
-cd "$DEPLOYMENT_DIR"
-
-configure_git_repo jenkins_home "$JENKINS_CONFIG_SEED_REPO" "${JENKINS_CONFIG_NEW_REPO:-NONE}" "${DEPLOY_JENKINS_CONFIG_SEED_REPO:-NONE}" "${DEPLOY_JENKINS_CONFIG_NEW_REPO:-NONE}"
-
-git_push_repo_cleanup jenkins_home
-
-INFO Setting up Jenkins to install required plugins
-cd "$DEPLOYMENT_DIR/jenkins_home"
-cp _init.groovy init.groovy
-
-# init.groovy will rename this when its run
-mv config.xml _config.xml
-
-cd ..
-
-configure_git_repo jenkins_scripts "$JENKINS_SCRIPTS_REPO" "${DEPLOY_JENKINS_SCRIPTS_REPO:-NONE}"
-
-cd "$DEPLOYMENT_DIR"
-
-INFO 'Installing initial plugin(s)'
-[ -d jenkins_home/plugins ] || mkdir -p jenkins_home/plugins
-
-cd "$DEPLOYMENT_DIR/jenkins_home/plugins"
-
-download_plugins ${PLUGINS:-$DEFAULT_PLUGINS}
-
-cd "$DEPLOYMENT_DIR"
-
-download_jenkins_war "$JENKINS_RELEASE_TYPE"
-
-INFO "Checking if all required packages are installed - this may take a while"
+INFO 'Checking if all required packages are installed - this may take a while'
 for _i in $BASE_PACKAGES; do
 	if ! rpm --quiet -q "$_i"; then
-		INFO . Installing $_i
+		INFO ". Installing $_i"
 		yum install -q -y "$_i"
 	fi
 done
 
 if [ -n "$FIX_SELINUX" ]; then
-	INFO Determining SELinux status
+	INFO 'Determining SELinux status'
 	sestatus 2>&1 >/dev/null && SELINUX_ENABLED=true
 fi
 
 INFO "Checking if we need to add the '$JENKINS_USER' user"
 if ! id $JENKINS_USER 2>&1 >/dev/null; then
-	INFO Adding $JENKINS_USER
+	INFO "Adding $JENKINS_USER"
 	useradd -d "$DEPLOYMENT_DIR" -r -s /sbin/nologin "$JENKINS_USER"
 fi
 
-cd bin
+cd "$DEPLOYMENT_DIR"
 
-INFO Extracting Jenkins CLI
-unzip -qqj "$DEPLOYMENT_DIR/jenkins-$JENKINS_RELEASE_TYPE.war" WEB-INF/jenkins-cli.jar
+if [ -n "$JENKINS_MASTER_URL" ]; then
+	INFO 'Deploying Jenkins slave'
+	JENKINS_APPNAME="$JENKINS_SLAVE-slave"
 
-cd - 2>&1 >/dev/null
+	curl -Lo "$DEPLOYMENT_DIR/bin/agent.jar" "$JENKINS_MASTER_URL/jnlpJars/agent.jar"
+else
+	INFO 'Deploying Jenkins master'
+	configure_git_repo jenkins_home "$JENKINS_CONFIG_SEED_REPO" "${JENKINS_CONFIG_NEW_REPO:-NONE}" "${DEPLOY_JENKINS_CONFIG_SEED_REPO:-NONE}" "${DEPLOY_JENKINS_CONFIG_NEW_REPO:-NONE}"
 
-INFO Creating required directories
+	git_push_repo_cleanup jenkins_home
+
+	INFO 'Setting up Jenkins to install required plugins'
+	cd "$DEPLOYMENT_DIR/jenkins_home"
+	cp _init.groovy init.groovy
+
+	# init.groovy will rename this when its run
+	mv config.xml _config.xml
+
+	cd ..
+
+	configure_git_repo jenkins_scripts "$JENKINS_SCRIPTS_REPO" "${DEPLOY_JENKINS_SCRIPTS_REPO:-NONE}"
+
+	cd "$DEPLOYMENT_DIR"
+
+	INFO 'Installing initial plugin(s)'
+	[ -d jenkins_home/plugins ] || mkdir -p jenkins_home/plugins
+
+	cd "$DEPLOYMENT_DIR/jenkins_home/plugins"
+
+	download_plugins ${PLUGINS:-$DEFAULT_PLUGINS}
+
+	cd "$DEPLOYMENT_DIR"
+
+	download_jenkins_war "$JENKINS_RELEASE_TYPE"
+
+	cd bin
+
+	INFO 'Extracting Jenkins CLI'
+	unzip -qqj "$DEPLOYMENT_DIR/jenkins-$JENKINS_RELEASE_TYPE.war" WEB-INF/jenkins-cli.jar
+
+	cd - 2>&1 >/dev/null
+
+	INFO 'Creating httpd reverse proxy setup'
+	cat >/etc/httpd/conf.d/$JENKINS_APPNAME-proxy.conf <<EOF
+	ProxyPass         "/" "http://127.0.0.1:8080/"
+	ProxyPassReverse  "/" "http://127.0.0.1:8080/"
+EOF
+
+	if [ -n "$FIX_FIREWALL" ]; then
+		INFO 'Permitting access to HTTP'
+		firewall-cmd --add-service=http --permanent
+
+		INFO Reloading firewall
+		firewall-cmd --reload
+	fi
+
+INFO
+fi
+
+INFO 'Configuring SSH'
 [ -d ~/.ssh ] || mkdir -p 0700 ~/.ssh
 
 # Suck in the SSH keys for our Git repos - we also add it to ~/.ssh/known_hosts to silence
@@ -192,21 +225,19 @@ for i in $SSH_KEYSCAN_HOSTS; do
 
 done | sort -u | tee -a ~/.ssh/known_hosts >>"$DEPLOYMENT_DIR/.ssh/known_hosts"
 
-INFO Fixing any duplicate known_hosts entries
+INFO 'Fixing any duplicate known_hosts entries'
 for _d in ~/.ssh/known_hosts "$DEPLOYMENT_DIR/.ssh/known_hosts"; do
 	FIX_DUPLICATES="`mktemp /tmp/SSH.XXXX`"
 
 	sort -u "$DEPLOYMENT_DIR/.ssh/known_hosts" >"$FIX_DUPLICATES"
 
 	if ! diff -q "$FIX_DUPLICATES" ~/.ssh/known_hosts 2>&1 >/dev/null; then
-		INFO Removing duplicates
+		INFO 'Removing duplicates'
 		mv -f "$FIX_DUPLICATES" ~/.ssh/known_hosts
 	fi
 
 	[ -f "$FIX_DUPLICATES" ] && rm -rf "$FIX_DUPLICATES"
 done
-
-
 
 INFO "Creating $JENKINS_APPNAME configuration"
 cat >"$DEPLOYMENT_DIR/config/$JENKINS_APPNAME.config" <<EOF
@@ -216,19 +247,19 @@ export JENKINS_CLI_JAR="$DEPLOYMENT_DIR/bin/jenkins-cli.jar"
 export JENKINS_LOCATION="localhost:8080"
 EOF
 
-INFO "Checking if we need to set a proxy"
+INFO 'Checking if we need to set a proxy'
 if [ -f /etc/wgetrc ] && grep '^http_proxy *= *' /etc/wgetrc; then
 	sed -e $SED_OPT 's/^http_proxy *= *(.*) *$/export http_proxy='"'"'\1'"'"'/g' /etc/wgetrc >>"$DEPLOYMENT_DIR/config/$JENKINS_APPNAME.config"
 fi
 
-INFO Creating startup script
-cat>>"$DEPLOYMENT_DIR/bin/$JENKINS_APPNAME-startup.sh" <<EOF
+INFO 'Creating startup script'
+cat >>"$DEPLOYMENT_DIR/bin/$JENKINS_APPNAME-startup.sh" <<EOF
 #!/bin/sh
 
 set -e
 
 if [ x"$JENKINS_USER" != x"\$USER" ]; then
-	echo FATAL This startup script MUST be run as $JENKINS_USER
+	FATAL 'This startup script MUST be run as $JENKINS_USER'
 
 	exit 1
 fi
@@ -236,15 +267,19 @@ fi
 [ -f "/etc/sysconfig/$JENKINS_APPNAME" ] && . /etc/sysconfig/$JENKINS_APPNAME
 
 [ -f "/var/log/$JENKINS_APPNAME/$JENKINS_APPNAME.log" ] && gzip -c "/var/log/$JENKINS_APPNAME/$JENKINS_APPNAME.log" >"/var/log/$JENKINS_APPNAME/$JENKINS_APPNAME.log.previous.gz"
-
-java -Djava.awt.headless=true -jar "$DEPLOYMENT_DIR/jenkins-$JENKINS_RELEASE_TYPE.war" --httpListenAddress=127.0.0.1 >/var/log/$JENKINS_APPNAME/$JENKINS_APPNAME.log 2>&1
 EOF
 
-INFO Creating httpd reverse proxy setup
-cat >/etc/httpd/conf.d/$JENKINS_APPNAME-proxy.conf <<EOF
-ProxyPass         "/" "http://127.0.0.1:8080/"
-ProxyPassReverse  "/" "http://127.0.0.1:8080/"
+if [ -z "$JENKINS_MASTER_URL" ]; then
+	# We are a master
+	cat >>"$DEPLOYMENT_DIR/bin/$JENKINS_APPNAME-startup.sh" <<EOF
+java -Djava.awt.headless=true -jar "$DEPLOYMENT_DIR/jenkins-$JENKINS_RELEASE_TYPE.war" --httpListenAddress=127.0.0.1 >"/var/log/$JENKINS_APPNAME/$JENKINS_APPNAME.log" 2>&1
 EOF
+else
+	# We are a slave
+	cat >>"$DEPLOYMENT_DIR/bin/$JENKINS_APPNAME-startup.sh" <<EOF
+java -Djava.awt.headless=true -jar "$JENKINS_AGENT_JAR" -workDir "$DEPLOYMENT_DIR/work" >"/var/log/$JENKINS_APPNAME/$JENKINS_APPNAME.log" 2>&1
+EOF
+fi
 
 INFO "Creating $JENKINS_APPNAME systemd service"
 cat >"$DEPLOYMENT_DIR/config/$JENKINS_APPNAME.service" <<EOF
@@ -261,7 +296,7 @@ User=$JENKINS_USER
 WantedBy=multi-user.target
 EOF
 
-INFO Generating logrotate config
+INFO 'Generating logrotate config'
 cat >"$DEPLOYMENT_DIR/config/$JENKINS_APPNAME.rotate" <<EOF
 # Logrotate for $JENKINS_APPNAME
 
@@ -284,7 +319,7 @@ if [ -f "/usr/lib/systemd/system/$JENKINS_APPNAME.service" ]; then
 	RELOAD_SYSTEMD=1
 fi
 
-INFO Generating SSH keys
+INFO 'Generating SSH keys'
 ssh-keygen -qt rsa -f "$DEPLOYMENT_DIR/.ssh/id_rsa" -N '' -C "$JENKINS_APPNAME"
 
 INFO Installing service
@@ -295,57 +330,48 @@ if [ -n "$RELOAD_SYSTEMD" ]; then
 	systemctl -q daemon-reload
 fi
 
-INFO Installing config
+INFO 'Installing config'
 cp --no-preserve=mode -f "$DEPLOYMENT_DIR/config/$JENKINS_APPNAME.config" "/etc/sysconfig/$JENKINS_APPNAME"
 
-INFO Install logrotate config
+INFO 'Install logrotate config'
 cp --no-preserve=mode -f "$DEPLOYMENT_DIR/config/$JENKINS_APPNAME.rotate" /etc/logrotate.d
 
-INFO Ensuring we have the correct ownership
+INFO 'Ensuring we have the correct ownership'
 chown -R "$ROOT_USER:$ROOT_GROUP" "$DEPLOYMENT_DIR"
 chown -R "$JENKINS_USER:$JENKINS_GROUP" "$DEPLOYMENT_DIR/.ssh" "$DEPLOYMENT_DIR/.git" "$DEPLOYMENT_DIR/jenkins_home" "$DEPLOYMENT_DIR/jenkins_scripts"
 chown -R "$JENKINS_USER:$JENKINS_GROUP" /var/log/$JENKINS_APPNAME
 
-INFO Ensuring installation has the correct permissions
+INFO 'Ensuring installation has the correct permissions'
 chmod 0700 "$DEPLOYMENT_DIR/.ssh"
 chmod 0600 "$DEPLOYMENT_DIR/.ssh/id"*
 
 if [ -n "$SELINUX_ENABLED" ]; then
-	INFO Fixing SELinux permissions
+	INFO 'Fixing SELinux permissions'
 	chcon --reference=/etc/sysconfig/network "/etc/sysconfig/$JENKINS_APPNAME"
 	chcon --reference=/usr/lib/systemd/system/system.slice "/usr/lib/systemd/system/$JENKINS_APPNAME.service"
 
-	INFO Enabling SELinux reverse proxy permissions
+	INFO 'Enabling SELinux reverse proxy permissions'
 	setsebool -P httpd_can_network_connect 1
 fi
 
 
-INFO "Enabling and starting our services - Jenkins will install required plugins and restart a few times, so this may take a while"
+INFO 'Enabling and starting our services - Jenkins will install required plugins and restart a few times, so this may take a while'
 chmod 0755 "$DEPLOYMENT_DIR/bin/$JENKINS_APPNAME-startup.sh"
 systemctl enable $JENKINS_APPNAME.service
 systemctl enable httpd
 systemctl start $JENKINS_APPNAME.service
 systemctl start httpd
 
-if [ -n "$FIX_FIREWALL" ]; then
-	INFO Permitting access to HTTP
-	firewall-cmd --add-service=http --permanent
-
-	INFO Reloading firewall
-	firewall-cmd --reload
-fi
-
+INFO 'Jenkins should be available shortly'
 INFO
-INFO Jenkins should be available shortly
+INFO 'Please wait whilst things startup...'
 INFO
-INFO Please wait whilst things startup...
+INFO 'Whilst things are starting up you can add Jenkins public key to the Git repo(s)'
 INFO
-INFO "Whilst things are starting up you can add Jenkins public key to the Git repo(s)"
-INFO
-INFO SSH public key:
+INFO 'SSH public key:'
 cat "$DEPLOYMENT_DIR/.ssh/id_rsa.pub"
 INFO
-INFO "Jenkins is available on the following URL(s):"
+INFO 'Jenkins is available on the following URL(s):'
 INFO
 ip addr list | awk '/inet / && !/127.0.0.1/{gsub("/24",""); printf("http://%s\n",$2)}'
 INFO

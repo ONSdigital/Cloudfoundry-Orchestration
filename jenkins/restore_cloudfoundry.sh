bf4ce93a198e02f0c943f23e0544f8c9b3b1f06b
Robert Kent <git@rooted.ltd.uk>
2018-01-31 14:31:28 +0000

    Add backup & restore CF scripts

---
+++ jenkins/restore_cloudfoundry.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+#
+# Variables:
+#	DEPLOYMENT_NAME=[Deployment Name]
+#	S3_BACKUP_BUCKET=S3 Bucket Name
+
+set -e
+
+
+###########################################################
+#
+# Functionality shared between all of the Jenkins deployment scripts
+BASE_DIR="`dirname $0`"
+CF_PREAMBLE="$BASE_DIR/cloudfoundry-preamble.sh"
+
+if [ ! -f "$CF_PREAMBLE" ]; then
+	echo "Unable to find $CF_PREAMBLE"
+
+	exit 1
+fi
+
+"$CF_PREAMBLE"
+###########################################################
+
+./Scripts/bin/backup_cloudfoundry-s3.sh "$DEPLOYMENT_NAME" restore "s3://$S3_BACKUP_BUCKET/$DEPLOYMENT_NAME/s3_backups"
+

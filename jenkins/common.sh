9490b8c6fad12be6a222187d0a7f92c61c1c87d5
Robert Kent <git@rooted.ltd.uk>
2018-01-31 12:34:19 +0000

    Add branch_to_name() that generates a deployment name based on a branch name

--- jenkins/common.sh
+++ jenkins/common.sh
@@ -20,6 +20,19 @@ INFO(){
 	printf "%s" "$NONE_COLOUR"
 }
 
+branch_to_name(){
+	# Generates a deployment name from a given branch name - effectively, it just takes the actual
+	# branch name and returns that as the deployment name
+	local branch_name="$1"
+
+	[ -z "$branch_name" ] || FATAL 'No Git branch name provided'
+
+	# sed $SED_OPT -e 's,^.*/([^/]+)$,\1,g' would also have done the job
+	local deployment_name="`basename "$branch_name"`"
+
+	[ -n "$deployment_name" ] || FATAL "Unable to determine deployment name from '$branch_name'"
+}
+
 #############################################
 # Detect the SED variant - this is only really useful when running jenkins/jenkins_deploy.sh
 # Some BSD sed variants don't handle -r they use -E for extended regular expression

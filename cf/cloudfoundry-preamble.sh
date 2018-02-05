22036a4a2871e65094ca01336353b9c4349829da
Robert Kent <git@rooted.ltd.uk>
2018-02-05 15:20:20 +0000

    Fix logic

--- cf/cloudfoundry-preamble.sh
+++ cf/cloudfoundry-preamble.sh
@@ -41,7 +41,7 @@ DEPLOYMENT_NAME="`branch_to_name "$GIT_BRANCH"`"
 
 # For AWS these are generated automatically
 # For VMware these are hand crafted
-[ ! -d "deployment/$DEPLOYMENT_NAME/outputs" ] || FATAL 'Deployment outputs do not exist'
+[ -d "deployment/$DEPLOYMENT_NAME/outputs" ] || FATAL 'Deployment outputs do not exist'
 
 if [ x"$SKIP_CF_SETUP" != x'true' -a ! -f "deployment/$DEPLOYMENT_NAME/cf-credentials-admin.sh" ] && [ -z "$ADMIN_EMAIL_ADDRESS" ]; then
 	FATAL 'No admin email address provided'

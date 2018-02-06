50f418a749a690294f7d72db4f2b9d4e826b0430
Robert Kent <git@rooted.ltd.uk>
2018-02-06 16:21:44 +0000

    Adjust things slightly to have better names

--- misc/repo_setup.sh
+++ misc/repo_setup.sh
@@ -21,25 +21,34 @@ fi
 . "$COMMON_SH"
 ###########################################################
 
-DEFAULT_GIT_REPO='Cloudfoundry-Deployment'
+DEFAULT_LOCAL_DIR='Cloudfoundry-Deployment'
 DEFAULT_GIT_BASE_URL='https://github.com/ONSdigital'
+DEFAULT_REPOS='AWS-Cloudformation Bosh-Manifests Scripts postgresql-databases-release'
 
+# Remote repository to push the newly created repo to - must be an empty, or use 'fake' to create a fake local repo
 REMOTE_REPO="${1:-$REMOTE_REPO}"
-GIT_REPO="${2:-${GIT_REPO:-$DEFAULT_GIT_REPO}}"
+# Local Git repository dir name
+LOCAL_DIR="${2:-${LOCAL_DIR:-$DEFAULT_LOCAL_DIR}}"
+# Base URL that we add $REPOS_NAMES to
 GIT_BASE_URL="${3:-${GIT_BASE_URL:-$DEFAULT_GIT_BASE_URL}}"
 
-REPOS='AWS-Cloudformation Bosh-Manifests Scripts'
-RELEASE_REPOS='postgresql-databases-release'
+if [ -n "$4" ]; then
+	shift 3
+	#
+	REPO_NAMES="$@"
+else
+	REPO_NAMES="$DEFAULT_REPOS"
+fi
 
 [ -z "$REMOTE_REPO" ] && FATAL 'No remote repository provided'
-[ -d "$GIT_REPO" ] && FATAL "Existing '$GIT_REPO' folder exists"
+[ -d "$LOCAL_DIR" ] && FATAL "Existing '$LOCAL_DIR' folder exists"
 
 if [ x"$REMOTE_REPO" = x'fake' ]; then
 	INFO 'Creating fake local repository'
 
-	mkdir -p "$GIT_REPO-local"
+	mkdir -p "$LOCAL_DIR-local"
 
-	cd "$GIT_REPO-local"
+	cd "$LOCAL_DIR-local"
 
 	REMOTE_REPO="file:///$PWD"
 
@@ -48,10 +57,10 @@ if [ x"$REMOTE_REPO" = x'fake' ]; then
 	cd -
 fi
 
-INFO "Creating $GIT_REPO"
-mkdir -p "$GIT_REPO"
+INFO "Creating $LOCAL_DIR"
+mkdir -p "$LOCAL_DIR"
 
-cd "$GIT_REPO"
+cd "$LOCAL_DIR"
 
 git init
 
@@ -61,18 +70,20 @@ git remote add origin "$REMOTE_REPO"
 
 mkdir -p vendor
 
-for _r in $REPOS; do
+for _r in $REPO_NAMES; do
 	INFO "Adding $_r repository"
-	git submodule add "$GIT_BASE_URL/Cloudfoundry-$_r" "vendor/$_r"
-done
 
-for _r in "$RELEASE_REPOS"; do
-	INFO "Adding $_r release repository"
-	git submodule add "$GIT_BASE_URL/$_r" "vendor/$_r"
+	if echo "$_r" | grep -Eq -- '-release'; then
+		# Releases go under their own folder
+		git submodule add "$GIT_BASE_URL/releases/$_r" "vendor/$_r"
+	else
+		# We strip off the Cloudfoundry- prefix
+		git submodule add "$GIT_BASE_URL/Cloudfoundry-$_r" "vendor/$_r"
+	fi
 done
 
 INFO "Pushing changes to $REMOTE_REPO"
 git commit -m 'Initial repository setup'
 git push --all
 
-INFO "Local checkout of $REMOTE_REPO is available: $GIT_REPO"
+INFO "Local checkout of $REMOTE_REPO is available: $LOCAL_DIR"

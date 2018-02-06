c2eb7dcf338bd3ca7caf6cf9ee36e86705a815ee
Robert Kent <git@rooted.ltd.uk>
2018-02-06 12:10:36 +0000

    Allow updating of existing quotas

--- cf-cli/create-quota.sh
+++ cf-cli/create-quota.sh
@@ -32,11 +32,12 @@ fi
 ###########################################################
 
 # Defaults
+DEFAULT_QUOTA_NAME='default'
 DEFAULT_TOTAL_MEMORY='24G'
 DEFAULT_TOTAL_ROUTES='48'
 DEFAULT_TOTAL_SERVICES='48'
 
-QUOTA_NAME="${QUOTA_NAME:-$1}"
+QUOTA_NAME="${1:-${QUOTA_NAME:-$DEFAULT_QUOTA_NAME}}"
 
 [ -n "$1" ] || WARN "No quota name provided, creating 'default' quota"
 
@@ -44,5 +45,13 @@ TOTAL_MEMORY="${2:-${TOTAL_MEMORY:-$DEFAULT_TOTAL_MEMORY}}"
 TOTAL_ROUTES="${3:-${TOTAL_ROUTES:-$DEFAULT_TOTAL_ROUTES}}"
 TOTAL_SERVICES="${4:-${TOTAL_SERVICES:-$DEFAULT_TOTAL_SERVICES}}"
 
-INFO "Creating quota '$QUOTA_NAME' with '$TOTAL_MEMORY' total memory, '$TOTAL_ROUTES' total routes and '$TOTAL_SERVICES' total number of services"
-"$CF" create-quota "$QUOTA_NAME" --allow-paid-service-plans -m "$TOTAL_MEMORY" -r "$TOTAL_ROUTES" -s "$TOTAL_SERVICES"
+if "$CF" quota "$QUOTA_NAME"; then
+	LOG_MSG='Updating'
+	ACTION='update'
+else
+	LOG_MSG='Creating'
+	ACTION='create'
+fi
+
+INFO "$LOG_MSG quota '$QUOTA_NAME' with '$TOTAL_MEMORY' total memory, '$TOTAL_ROUTES' total routes and '$TOTAL_SERVICES' total number of services"
+"$CF" $ACTION-quota "$QUOTA_NAME" --allow-paid-service-plans -m "$TOTAL_MEMORY" -r "$TOTAL_ROUTES" -s "$TOTAL_SERVICES"

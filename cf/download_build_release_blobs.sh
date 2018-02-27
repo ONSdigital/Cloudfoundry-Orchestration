b5b6e333340d1c557c78f7f97b39e3e243019b9d
Robert Kent <git@rooted.ltd.uk>
2018-02-27 18:54:25 +0000

    Add script to download/build blobs

---
+++ cf/download_build_release_blobs.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+#
+# Variables:
+#
+set -e
+
+
+###########################################################
+#
+# Functionality shared between all of the Jenkins deployment scripts
+BASE_DIR="`dirname $0`"
+COMMON_SH="$BASE_DIR/../common/common.sh"
+
+if [ ! -f "$COMMON_SH" ]; then
+	echo "Unable to find $COMMON_SH"
+
+	exit 1
+fi
+
+. "$COMMON_SH"
+###########################################################
+
+install_scripts
+
+ACTION="${1:-build}"
+
+grep -Eq '^(build|download)$' <<EOF || FATAL "Incorrect action. Valid action: build or download'
+$ACTION
+EOF
+
+[ -d releases ] && subdir='vendor' || subdir='releases'
+
+for _d in `find $subdir-name \*release`; do
+	find $_d -name "${ACTION}_blobs.sh" -exec {} \;
+done

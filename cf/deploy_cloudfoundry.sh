e56faefbba028e798a7d265b5fb3de9932ce71eb
Robert Kent <git@rooted.ltd.uk>
2018-02-27 18:07:03 +0000

    Try to automatically determine Bosh/CF CLIs

--- cf/deploy_cloudfoundry.sh
+++ cf/deploy_cloudfoundry.sh
@@ -35,13 +35,21 @@ if [ -z "$GIT_COMMIT_MESSAGE" ]; then
 	[ x"$CLOUDFOUNDRY_DEPLOYMENT_BRANCH" = x'origin/master' ] && GIT_COMMIT_MESSAGE="New deployment $DEPLOYMENT_NAME" || GIT_COMMIT_MESSAGE="Updated deployment $DEPLOYMENT_NAME"
 fi
 
+# CF & Bosh CLIs may have a version suffix
+[ -f work/bin/bosh ] && BOSH_CLI='work/bin/bosh' || BOSH_CLI="`find work/bin -name bosh-\*`"
+[ -f work/bin/cf ] && CF_CLI='work/bin/cf' || CF_CLI="`find work/bin -name cf-\*`"
+
+[ -z "$CF_CLI" -o ! -f "$CF_CLI" ] || FATAL 'CF CLI does not exist'
+[ -z "$BOSH_CLI" -o ! -f "$BOSH_CLI" ] || FATAL 'BOSH CLI does not exist'
+
 # Deploy Cloudfoundry instance
 ./Scripts/bin/deploy_cloudfoundry.sh "$DEPLOYMENT_NAME" || FAILED=1
 
 # Generate admin credentials if we don't already have some
 #if [ -z "$FAILED" -a -n "$ADMIN_EMAIL_ADDRESS" -a -n "$SKIP_CF_SETUP" -a x"$SKIP_CF_SETUP" != x"true" ]; then
 if [ -z "$FAILED" ] && [ -z "$SKIP_CF_SETUP" -o x"$SKIP_CF_SETUP" = x"false" ]; then
-	./Scripts/bin/setup_cf.sh "$DEPLOYMENT_NAME" "${ADMIN_EMAIL_ADDRESS:-NONE}" || FAILED=1
+	#./Scripts/bin/setup_cf.sh "$DEPLOYMENT_NAME" "${ADMIN_EMAIL_ADDRESS:-NONE}" || FAILED=1
+	./Scripts/bin/setup_cf.sh "$DEPLOYMENT_NAME" || FAILED=1
 
 #	[ -f "deployment/$DEPLOYMENT_NAME/cf-credentials-admin.sh" ] && git add "deployment/$DEPLOYMENT_NAME/cf-credentials-admin.sh" 
 fi

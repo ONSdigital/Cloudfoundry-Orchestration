b74eac3a9b06050acd39ef69b8079952b892c12f
Robert Kent <git@rooted.ltd.uk>
2018-02-28 17:11:04 +0000

    Avoid using subdir in destination

--- cf/download_build_release_blobs.sh
+++ cf/download_build_release_blobs.sh
@@ -32,7 +32,9 @@ EOF
 [ -d releases ] && subdir='releases' || subdir='vendor'
 
 INFO 'Finding releases'
-for _d in `find $subdir -mindepth 1 -maxdepth 1 -type d -name \*release`; do
+cd $subdir
+
+for _d in `find . -mindepth 1 -maxdepth 1 -type d -name \*release`; do
 	if [ -x "$_d/bin/${ACTION}_blobs.sh" ]; then
 		INFO ". executing $_d/bin/${ACTION}_blobs.sh"
 		"$_d/bin/${ACTION}_blobs.sh" "blobs/$_d"
